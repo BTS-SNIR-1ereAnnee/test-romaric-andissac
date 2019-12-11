54111f31ba1b73e5630e1d24cf5c94ec1adb5c7e
romaric-andissac <andissac.romaric@lyceeppr.fr>
2019-12-11 14:56:03 +0100

    Ajout testlongueur

---
+++ testLongueur.cpp
@@ -0,0 +1,26 @@
+/*
+ chaine de caractères
+ essai sur la longueur d'une chaîne
+ */
+
+#include <iostream>
+
+
+
+using namespace std;
+
+int main() {
+
+   int x;
+   char mot[8];
+   
+   cout << "donnez un mot" << endl;
+   fgets(mot, sizeof(mot), stdin);
+   cout << mot << "" << endl;
+   
+  
+   
+  
+   return 0;
+}
+
